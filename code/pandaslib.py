d01a930973a14e6ead083a3a1fc804d099413b3b
ntbamber <ntbamber@syr.edu>
2025-03-28 13:40:41 -0400

    pandslib

--- code/pandaslib.py
+++ code/pandaslib.py
@@ -3,14 +3,16 @@ from datetime import datetime
 def clean_currency(item: str) -> float:
     '''
     remove anything from the item that prevents it from being converted to a float
-    '''    
-    return 
+    '''
+    cleaned_item = ''.join(char for char in item if char.isdigit() or char == '.')#Extracts all characters that are digits or .
+    return float(cleaned_item)    
 
 def extract_year_mdy(timestamp):
     '''
     use the datatime.strptime to parse the date and then extract the year
     '''
-    return 
+    date = datetime.strptime(timestamp, '%m/%d/%Y %H:%M:%S')  # parse the full timestamp
+    return date.year #return the year
 
 def clean_country_usa(item: str) ->str:
     '''
@@ -18,9 +20,14 @@ def clean_country_usa(item: str) ->str:
     with 'United States'
     '''
     possibilities = [
-        'united states of america', 'usa', 'us', 'united states', 'u.s.'
+        'united states of america', 'u sa', 'us', 'united states', 'u.s.', 'usa'
     ]
-    return
+    item_lower = item.lower().strip() #standardizes input to lowercase and removes leading/trailing whitespaces
+    if item_lower in possibilities: #checks if item is in possibilities
+        return 'United States' #returns 'United States'
+    else:
+        # Return the original item if no match is found
+        return item
 
 
 if __name__=='__main__':
